dd74374530444100f847c1bfcdcb0d07909c527d
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-11-27 20:23:24 +0900

    KOF -> KOT로 수정
    킹오파에서 킹오탱으로..
    
    참조하고 있는 부분들도 다 바꾼 후 정상 실행됩니다.

--- KOT_Scene.h
+++ KOT_Scene.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "Scene.h"
-class KOF_Scene: public Scene
+class KOT_Scene: public Scene
 {
 public:
-	KOF_Scene();
+	KOT_Scene();
 
 	void timer(float delta) override;
 	void event(unsigned char key, int x, int y) override;
